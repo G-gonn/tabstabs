f50381681eb4b7d1681b9b6688ef5c75511a73c0
G-gonn <skander.apollo@gmail.com>
2017-11-23 19:43:46 +0100

    Initial commit

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,21 @@
+jQuery(function($){
+
+	$(".tabs").each(function(){
+		var current = null;
+		current = $(this).find("a").attr("href");
+		$(this).find('a[href="'+current+'"]').addClass("active");
+		$(current).siblings().hide();
+		$(this).find("a").click(function(){
+				var link = $(this).attr("href");
+			if(link == current){
+				return false;
+			}else{
+				$(this).siblings().removeClass("active");
+				$(this).addClass("active");
+				$(link).show().siblings().hide();
+				current = link;
+			}
+		});
+	});
+
+});
